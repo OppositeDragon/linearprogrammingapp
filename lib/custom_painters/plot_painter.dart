a040cd99b86ad034768f27544b50f0b53671893d
OppositeDragon <stee-ven_94@outlook.com>
2023-07-14 20:33:22 -0500

    LPOR-9 - x-axis and y-axis are draw unig the drawLine API.

--- lib/custom_painters/plot_painter.dart
+++ lib/custom_painters/plot_painter.dart
@@ -1,14 +1,31 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+extension Num on num {
+  int length() => toString().length;
+}
 
 class PlotPainter extends CustomPainter {
   PlotPainter({required this.theme, required this.limitX, required this.limitY});
   final ThemeData theme;
   final double limitX;
   final double limitY;
+
+  double getRoundedMaxValue(double maximum) {
+    double magnitude = (log(maximum) / log(10)).floor().toDouble();
+    double roundingFactor = pow(10, magnitude).toDouble();
+    double roundedRange = (maximum / roundingFactor).ceil().toDouble();
+    double roundedMaximum = roundedRange * roundingFactor;
+    print('desiredMaximum: $roundedMaximum');
+    return roundedMaximum;
+  }
+
   @override
   void paint(Canvas canvas, Size size) {
-    final numerationLimitX = (limitX / 10).ceil() * 10;
+    double numerationLimitX = getRoundedMaxValue(301); //(limitX / 100).ceil() * 10;
     final numerationLimitY = (limitY / 10).ceil() * 10;
+
     final textStyle = theme.textTheme.bodyMedium!.copyWith(fontFamily: 'Computer Modern');
 
     final textPainterX = TextPainter(
@@ -18,45 +35,50 @@ class PlotPainter extends CustomPainter {
     final rightMargin = 25.0 + textPainterX.width / 2;
     const topMargin = 30.0;
     const bottomMargin = 30.0;
-    final ceilX = (limitX / 10).ceil();
 
     const segmentsX = 5;
     const segmentsY = 5;
     //calculate offsets
-    final hOffset = (size.width - leftMargin - rightMargin) / segmentsX;
+    final offsetX = (size.width - leftMargin - rightMargin) / segmentsX;
 
     final paint = Paint()
-      ..color = Colors.black
+      ..color = Colors.purple
       ..strokeWidth = 1
       ..style = PaintingStyle.stroke;
 
-    final pathY = Path()
-      ..moveTo(leftMargin, size.height - 20)
-      ..lineTo(leftMargin, 20);
-    canvas.drawPath(pathY, paint);
     final rectagle = Rect.fromPoints(Offset.zero, Offset(size.width, size.height));
-    canvas.drawRect(rectagle, paint..color = Colors.yellow);
-    paint.color = Colors.red;
-    final pathX = Path()
-      ..moveTo(20, size.height - bottomMargin)
-      ..lineTo(size.width - 20, size.height - bottomMargin);
+    canvas.drawRect(rectagle, paint);
+    //y-axis
+    canvas.drawLine(
+      const Offset(leftMargin, topMargin),
+      Offset(leftMargin, size.height - bottomMargin / 1.5),
+      paint..color = Colors.yellow,
+    );
+    //x-axis
+    canvas.drawLine(
+      Offset(leftMargin / 1.5, size.height - bottomMargin),
+      Offset(size.width - rightMargin / 2, size.height - bottomMargin),
+      paint..color = Colors.red,
+    );
 
-    canvas.drawPath(pathX, paint);
-    final stepX = numerationLimitX ~/ 5;
-
-    for (int i = stepX; i <= numerationLimitX; i += stepX) {
+    // var stepX = (numerationLimitX * 1.1 / 10).toDouble();
+    // numerationLimitX = (stepX * 10);
+    var stepX = numerationLimitX / segmentsX;
+    //numerationLimitX = (stepX * 5).ceil().toDouble();
+    print('stepX: $stepX, numerationLimitX: $numerationLimitX');
+    for (int i = stepX.toInt(); i <= numerationLimitX; i += stepX.toInt()) {
       TextSpan span = TextSpan(style: textStyle, text: i.toString());
       final textPainter = TextPainter(
         text: span,
         textDirection: TextDirection.ltr,
       )..layout();
-      final double dx = ((i) * hOffset / stepX) + leftMargin - textPainter.width / 2;
+      final double dx = ((i) * offsetX / stepX) + leftMargin - textPainter.width / 2;
 
       textPainter.paint(canvas, Offset(dx, size.height - bottomMargin));
     }
     paint.color = Colors.blue;
-    for (int i = 0; i <= numerationLimitX; i += stepX) {
-      final double dx = ((i) * hOffset / stepX) + leftMargin;
+    for (int i = 0; i <= numerationLimitX; i += stepX.toInt()) {
+      final double dx = ((i) * offsetX / stepX) + leftMargin;
       canvas.drawLine(Offset(dx, size.height - bottomMargin - 5), Offset(dx, size.height - bottomMargin + 5), paint);
     }
   }
