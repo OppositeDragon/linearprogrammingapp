c2088c4092ef0324c3b7205539175acb39f211fc
OppositeDragon <stee-ven_94@outlook.com>
2023-11-05 07:49:23 -0500

    LPOR-9 - A new MOdel has been added. Aims to allow the calculation of the graphic answer on a controller, instead of the CustomPainter. Typedef have been created as well.

---
+++ lib/models/graphic_data_model.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'graphic_data_model.freezed.dart';
+
+typedef PointPair = ({Point<double> p1, Point<double> p2});
+typedef NullablePoints = ({Point<double>? p1, Point<double>? p2});
+
+@freezed
+class GraphicDataModel with _$GraphicDataModel {
+  const factory GraphicDataModel({
+    required double xLimit,
+    required double yLimit,
+    required Point<double> answer,
+    required PointPair answerObjectiveFunction,
+    required List<PointPair> restrictions,
+    required List<List<Point<double>>> feasibleRegionMatrixPoints,
+    @Default('feasible\nregion') String feasibleRegionText,
+  }) = _GraphicDataModel;
+}
---
+++ lib/models/graphic_data_model.freezed.dart
@@ -0,0 +1,293 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'graphic_data_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$GraphicDataModel {
+  double get xLimit => throw _privateConstructorUsedError;
+  double get yLimit => throw _privateConstructorUsedError;
+  Point<double> get answer => throw _privateConstructorUsedError;
+  ({Point<double> p1, Point<double> p2}) get answerObjectiveFunction =>
+      throw _privateConstructorUsedError;
+  List<({Point<double> p1, Point<double> p2})> get restrictions =>
+      throw _privateConstructorUsedError;
+  List<List<Point<double>>> get feasibleRegionMatrixPoints =>
+      throw _privateConstructorUsedError;
+  String get feasibleRegionText => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $GraphicDataModelCopyWith<GraphicDataModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $GraphicDataModelCopyWith<$Res> {
+  factory $GraphicDataModelCopyWith(
+          GraphicDataModel value, $Res Function(GraphicDataModel) then) =
+      _$GraphicDataModelCopyWithImpl<$Res, GraphicDataModel>;
+  @useResult
+  $Res call(
+      {double xLimit,
+      double yLimit,
+      Point<double> answer,
+      ({Point<double> p1, Point<double> p2}) answerObjectiveFunction,
+      List<({Point<double> p1, Point<double> p2})> restrictions,
+      List<List<Point<double>>> feasibleRegionMatrixPoints,
+      String feasibleRegionText});
+}
+
+/// @nodoc
+class _$GraphicDataModelCopyWithImpl<$Res, $Val extends GraphicDataModel>
+    implements $GraphicDataModelCopyWith<$Res> {
+  _$GraphicDataModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? xLimit = null,
+    Object? yLimit = null,
+    Object? answer = null,
+    Object? answerObjectiveFunction = null,
+    Object? restrictions = null,
+    Object? feasibleRegionMatrixPoints = null,
+    Object? feasibleRegionText = null,
+  }) {
+    return _then(_value.copyWith(
+      xLimit: null == xLimit
+          ? _value.xLimit
+          : xLimit // ignore: cast_nullable_to_non_nullable
+              as double,
+      yLimit: null == yLimit
+          ? _value.yLimit
+          : yLimit // ignore: cast_nullable_to_non_nullable
+              as double,
+      answer: null == answer
+          ? _value.answer
+          : answer // ignore: cast_nullable_to_non_nullable
+              as Point<double>,
+      answerObjectiveFunction: null == answerObjectiveFunction
+          ? _value.answerObjectiveFunction
+          : answerObjectiveFunction // ignore: cast_nullable_to_non_nullable
+              as ({Point<double> p1, Point<double> p2}),
+      restrictions: null == restrictions
+          ? _value.restrictions
+          : restrictions // ignore: cast_nullable_to_non_nullable
+              as List<({Point<double> p1, Point<double> p2})>,
+      feasibleRegionMatrixPoints: null == feasibleRegionMatrixPoints
+          ? _value.feasibleRegionMatrixPoints
+          : feasibleRegionMatrixPoints // ignore: cast_nullable_to_non_nullable
+              as List<List<Point<double>>>,
+      feasibleRegionText: null == feasibleRegionText
+          ? _value.feasibleRegionText
+          : feasibleRegionText // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$GraphicDataModelImplCopyWith<$Res>
+    implements $GraphicDataModelCopyWith<$Res> {
+  factory _$$GraphicDataModelImplCopyWith(_$GraphicDataModelImpl value,
+          $Res Function(_$GraphicDataModelImpl) then) =
+      __$$GraphicDataModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {double xLimit,
+      double yLimit,
+      Point<double> answer,
+      ({Point<double> p1, Point<double> p2}) answerObjectiveFunction,
+      List<({Point<double> p1, Point<double> p2})> restrictions,
+      List<List<Point<double>>> feasibleRegionMatrixPoints,
+      String feasibleRegionText});
+}
+
+/// @nodoc
+class __$$GraphicDataModelImplCopyWithImpl<$Res>
+    extends _$GraphicDataModelCopyWithImpl<$Res, _$GraphicDataModelImpl>
+    implements _$$GraphicDataModelImplCopyWith<$Res> {
+  __$$GraphicDataModelImplCopyWithImpl(_$GraphicDataModelImpl _value,
+      $Res Function(_$GraphicDataModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? xLimit = null,
+    Object? yLimit = null,
+    Object? answer = null,
+    Object? answerObjectiveFunction = null,
+    Object? restrictions = null,
+    Object? feasibleRegionMatrixPoints = null,
+    Object? feasibleRegionText = null,
+  }) {
+    return _then(_$GraphicDataModelImpl(
+      xLimit: null == xLimit
+          ? _value.xLimit
+          : xLimit // ignore: cast_nullable_to_non_nullable
+              as double,
+      yLimit: null == yLimit
+          ? _value.yLimit
+          : yLimit // ignore: cast_nullable_to_non_nullable
+              as double,
+      answer: null == answer
+          ? _value.answer
+          : answer // ignore: cast_nullable_to_non_nullable
+              as Point<double>,
+      answerObjectiveFunction: null == answerObjectiveFunction
+          ? _value.answerObjectiveFunction
+          : answerObjectiveFunction // ignore: cast_nullable_to_non_nullable
+              as ({Point<double> p1, Point<double> p2}),
+      restrictions: null == restrictions
+          ? _value._restrictions
+          : restrictions // ignore: cast_nullable_to_non_nullable
+              as List<({Point<double> p1, Point<double> p2})>,
+      feasibleRegionMatrixPoints: null == feasibleRegionMatrixPoints
+          ? _value._feasibleRegionMatrixPoints
+          : feasibleRegionMatrixPoints // ignore: cast_nullable_to_non_nullable
+              as List<List<Point<double>>>,
+      feasibleRegionText: null == feasibleRegionText
+          ? _value.feasibleRegionText
+          : feasibleRegionText // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$GraphicDataModelImpl implements _GraphicDataModel {
+  const _$GraphicDataModelImpl(
+      {required this.xLimit,
+      required this.yLimit,
+      required this.answer,
+      required this.answerObjectiveFunction,
+      required final List<({Point<double> p1, Point<double> p2})> restrictions,
+      required final List<List<Point<double>>> feasibleRegionMatrixPoints,
+      this.feasibleRegionText = 'feasible\nregion'})
+      : _restrictions = restrictions,
+        _feasibleRegionMatrixPoints = feasibleRegionMatrixPoints;
+
+  @override
+  final double xLimit;
+  @override
+  final double yLimit;
+  @override
+  final Point<double> answer;
+  @override
+  final ({Point<double> p1, Point<double> p2}) answerObjectiveFunction;
+  final List<({Point<double> p1, Point<double> p2})> _restrictions;
+  @override
+  List<({Point<double> p1, Point<double> p2})> get restrictions {
+    if (_restrictions is EqualUnmodifiableListView) return _restrictions;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_restrictions);
+  }
+
+  final List<List<Point<double>>> _feasibleRegionMatrixPoints;
+  @override
+  List<List<Point<double>>> get feasibleRegionMatrixPoints {
+    if (_feasibleRegionMatrixPoints is EqualUnmodifiableListView)
+      return _feasibleRegionMatrixPoints;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_feasibleRegionMatrixPoints);
+  }
+
+  @override
+  @JsonKey()
+  final String feasibleRegionText;
+
+  @override
+  String toString() {
+    return 'GraphicDataModel(xLimit: $xLimit, yLimit: $yLimit, answer: $answer, answerObjectiveFunction: $answerObjectiveFunction, restrictions: $restrictions, feasibleRegionMatrixPoints: $feasibleRegionMatrixPoints, feasibleRegionText: $feasibleRegionText)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$GraphicDataModelImpl &&
+            (identical(other.xLimit, xLimit) || other.xLimit == xLimit) &&
+            (identical(other.yLimit, yLimit) || other.yLimit == yLimit) &&
+            (identical(other.answer, answer) || other.answer == answer) &&
+            (identical(
+                    other.answerObjectiveFunction, answerObjectiveFunction) ||
+                other.answerObjectiveFunction == answerObjectiveFunction) &&
+            const DeepCollectionEquality()
+                .equals(other._restrictions, _restrictions) &&
+            const DeepCollectionEquality().equals(
+                other._feasibleRegionMatrixPoints,
+                _feasibleRegionMatrixPoints) &&
+            (identical(other.feasibleRegionText, feasibleRegionText) ||
+                other.feasibleRegionText == feasibleRegionText));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      xLimit,
+      yLimit,
+      answer,
+      answerObjectiveFunction,
+      const DeepCollectionEquality().hash(_restrictions),
+      const DeepCollectionEquality().hash(_feasibleRegionMatrixPoints),
+      feasibleRegionText);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$GraphicDataModelImplCopyWith<_$GraphicDataModelImpl> get copyWith =>
+      __$$GraphicDataModelImplCopyWithImpl<_$GraphicDataModelImpl>(
+          this, _$identity);
+}
+
+abstract class _GraphicDataModel implements GraphicDataModel {
+  const factory _GraphicDataModel(
+      {required final double xLimit,
+      required final double yLimit,
+      required final Point<double> answer,
+      required final ({
+        Point<double> p1,
+        Point<double> p2
+      }) answerObjectiveFunction,
+      required final List<({Point<double> p1, Point<double> p2})> restrictions,
+      required final List<List<Point<double>>> feasibleRegionMatrixPoints,
+      final String feasibleRegionText}) = _$GraphicDataModelImpl;
+
+  @override
+  double get xLimit;
+  @override
+  double get yLimit;
+  @override
+  Point<double> get answer;
+  @override
+  ({Point<double> p1, Point<double> p2}) get answerObjectiveFunction;
+  @override
+  List<({Point<double> p1, Point<double> p2})> get restrictions;
+  @override
+  List<List<Point<double>>> get feasibleRegionMatrixPoints;
+  @override
+  String get feasibleRegionText;
+  @override
+  @JsonKey(ignore: true)
+  _$$GraphicDataModelImplCopyWith<_$GraphicDataModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
