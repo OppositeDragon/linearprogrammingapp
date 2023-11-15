38b6d609ebce1a63030bca2bf6be1cecb5f65f89
OppositeDragon <stee-ven_94@outlook.com>
2023-11-14 20:48:13 -0500

    LPOR-21 - constants for routes and named routes are created. The router controller get updated to use those constants.

---
+++ lib/constants/routes.dart
@@ -0,0 +1,17 @@
+const String routeHome = '/';
+const String routeHomeName = 'home';
+
+const String routeLogin = '/login';
+const String routeLoginName = 'login';
+
+const String routeDataEntry = 'data-entry';
+const String routeDataEntryName = 'data-entry';
+
+const String routeAlgebraicProcess = 'algebraic-process';
+const String routeAlgebraicProcessName = 'algebraic-process';
+
+const String routeGraphicProcess = 'graphic-process';
+const String routeGraphicProcessName = 'graphic-process';
+
+const String routeSimplexProcess = 'simplex-process';
+const String routeSimplexProcessName = 'simplex-process';
