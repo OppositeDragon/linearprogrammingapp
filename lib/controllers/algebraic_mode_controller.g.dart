a6ac1e855c18b9ceac06fedf65fd9ed2ff187081
OppositeDragon <stee-ven_94@outlook.com>
2023-12-04 19:59:40 -0500

    LPOR-10 - AlgebraicController is on its own file, some methods are now extensions, or were moved to utils.

---
+++ lib/controllers/algebraic_mode_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'algebraic_mode_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$algebraicControllerHash() =>
+    r'faa94aaba9b4509e085f03b40a1115af4bf01ff5';
+
+/// See also [AlgebraicController].
+@ProviderFor(AlgebraicController)
+final algebraicControllerProvider =
+    AutoDisposeNotifierProvider<AlgebraicController, void>.internal(
+  AlgebraicController.new,
+  name: r'algebraicControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$algebraicControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AlgebraicController = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
