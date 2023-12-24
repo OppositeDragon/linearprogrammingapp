3e1425bede8a97bfee568411131d734e62a7932b
OppositeDragon <stee-ven_94@outlook.com>
2023-12-24 13:07:46 -0500

    LPOR-11 - Added the first phase to the simplex page. The `getCellColor` now is more readable, and works for 1st and 2nd phase.

--- lib/pages/process_simplex_page.dart
+++ lib/pages/process_simplex_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_math_fork/flutter_math.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,18 +15,19 @@ class SimplexProcessPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final TextTheme textTheme = Theme.of(context).textTheme;
-    final a = ref.read(simplexControllerProvider);
-    final simplexData = ref.read(simplexControllerProvider.notifier).getAnswer();
-
-    Color? cellColor(int i, int j, int k) {
-      if (i == simplexData.tableaus.length - 1 && j == 0 && k == simplexData.tableaus[i][j].length - 1) {
-        return Colors.tealAccent.withOpacity(0.25);
-      }
-      if (i != simplexData.tableaus.length - 1) {
-        if (simplexData.pivotsCoordinates[i].y == j && simplexData.pivotsCoordinates[i].x == k) {
+    final simplexData = ref.watch(simplexControllerProvider);
+    Color? getCellColor(int i, int j, int k, List<Point<int>> pivots, int dataLength, int rowLength) {
+      if (i == dataLength - 1) {
+        if (j == 0 && k == rowLength - 1) {
+          return Colors.tealAccent.withOpacity(0.25);
+        }
+      } else {
+        final int x = pivots[i].x;
+        final int y = pivots[i].y;
+        if (y == j && x + 1 == k) {
           return colorScheme.tertiary.withOpacity(0.2);
         }
-        if (simplexData.pivotsCoordinates[i].y == j || simplexData.pivotsCoordinates[i].x == k) {
+        if (y == j || x + 1 == k) {
           return colorScheme.primary.withOpacity(0.08);
         }
       }
@@ -38,9 +41,127 @@ class SimplexProcessPage extends ConsumerWidget {
       ),
       body: CustomScrollView(
         slivers: [
-          const SliverToBoxAdapter(
-            child: Text(''),
-          ),
+          if (simplexData.tableaus1st != null)
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: spaceXXL,
+                vertical: spaceXL,
+              ),
+              sliver: SliverToBoxAdapter(
+                child: Text(
+                  "Fase 1",
+                  style: textTheme.displayMedium!.copyWith(fontFamily: 'CMRomanSerif'),
+                ),
+              ),
+            ),
+          if (simplexData.tableaus1st != null)
+            SliverList.builder(
+              itemCount: simplexData.tableaus1st!.length,
+              itemBuilder: (context, i) {
+                return Center(
+                  child: SingleChildScrollView(
+                    primary: false,
+                    scrollDirection: Axis.horizontal,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(spaceS),
+                          child: Text(
+                            "Iteración ${i + 1}",
+                            style: textTheme.displaySmall!.copyWith(
+                              fontFamily: 'CMClassic', //fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                        DataTable(
+                          headingRowColor: const MaterialStatePropertyAll(Colors.black26),
+                          columnSpacing: 0,
+                          checkboxHorizontalMargin: 0,
+                          horizontalMargin: 0,
+                          columns: [
+                            const DataColumn(label: Text('')),
+                            for (int j = 0; j < simplexData.tableaus1st![i].last.length - 1; j++)
+                              DataColumn(
+                                label: Expanded(
+                                  child: Center(
+                                    child: j == simplexData.tableaus1st![i].last.length - 2
+                                        ? Text(
+                                            ' Lado\n  derecho  ',
+                                            textAlign: TextAlign.center,
+                                            style: textTheme.titleMedium!.copyWith(
+                                              fontFamily: 'CMRomanSerif',
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          )
+                                        : Math.tex(
+                                            'x_${j + 1}',
+                                            textStyle: textTheme.titleLarge,
+                                            mathStyle: MathStyle.display,
+                                          ),
+                                  ),
+                                ),
+                              ),
+                          ],
+                          rows: [
+                            for (int j = 0; j < simplexData.tableaus1st![i].length; j++)
+                              DataRow(
+                                cells: [
+                                  for (int k = 0; k < simplexData.tableaus1st![i][j].length; k++)
+                                    DataCell(
+                                      DecoratedBox(
+                                        decoration: BoxDecoration(
+                                          color: getCellColor(
+                                            i,
+                                            j,
+                                            k,
+                                            simplexData.pivotsCoordinates1st!,
+                                            simplexData.tableaus1st!.length,
+                                            -1,
+                                          ),
+                                        ),
+                                        child: SizedBox.expand(
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(horizontal: spaceXXXL),
+                                            child: Center(
+                                              child: k == 0
+                                                  ? Math.tex(simplexData.tableaus1st![i][j][k],
+                                                      textStyle: textTheme.titleLarge)
+                                                  : Text(
+                                                      simplexData.tableaus1st![i][j][k],
+                                                      style: textTheme.bodyLarge!.copyWith(
+                                                        fontFamily: 'CMRomanSerif',
+                                                        fontWeight: FontWeight.bold,
+                                                      ),
+                                                    ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                              ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          if (simplexData.tableaus1st != null)
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: spaceXXL,
+                vertical: spaceXL,
+              ),
+              sliver: SliverToBoxAdapter(
+                child: Text(
+                  "Fase 2",
+                  style: textTheme.displayMedium!.copyWith(fontFamily: 'CMRomanSerif'),
+                ),
+              ),
+            ),
           SliverList.builder(
             itemCount: simplexData.tableaus.length,
             itemBuilder: (context, i) {
@@ -54,7 +175,7 @@ class SimplexProcessPage extends ConsumerWidget {
                       Padding(
                         padding: const EdgeInsets.all(spaceS),
                         child: Text(
-                          "Paso ${i + 1}",
+                          "Iteración ${i + 1}",
                           style: textTheme.displaySmall!.copyWith(
                             fontFamily: 'CMClassic', //fontWeight: FontWeight.bold
                           ),
@@ -67,26 +188,27 @@ class SimplexProcessPage extends ConsumerWidget {
                         horizontalMargin: 0,
                         columns: [
                           const DataColumn(label: Text('')),
-                          for (int j = 0; j < simplexData.tableaus[i].first.length - 1; j++)
+                          for (int j = 0; j < simplexData.tableaus[i].last.length - 1; j++)
                             DataColumn(
-                                label: Expanded(
-                              child: Center(
-                                child: j == simplexData.tableaus[i].first.length - 2
-                                    ? Text(
-                                        ' Lado\n  derecho  ',
-                                        textAlign: TextAlign.center,
-                                        style: textTheme.titleMedium!.copyWith(
-                                          fontFamily: 'CMRomanSerif',
-                                          fontWeight: FontWeight.bold,
+                              label: Expanded(
+                                child: Center(
+                                  child: j == simplexData.tableaus[i].last.length - 2
+                                      ? Text(
+                                          ' Lado\n  derecho  ',
+                                          textAlign: TextAlign.center,
+                                          style: textTheme.titleMedium!.copyWith(
+                                            fontFamily: 'CMRomanSerif',
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        )
+                                      : Math.tex(
+                                          'x_${j + 1}',
+                                          textStyle: textTheme.titleLarge,
+                                          mathStyle: MathStyle.display,
                                         ),
-                                      )
-                                    : Math.tex(
-                                        'x_${j + 1}',
-                                        textStyle: textTheme.titleLarge,
-                                        mathStyle: MathStyle.display,
-                                      ),
+                                ),
                               ),
-                            )),
+                            ),
                         ],
                         rows: [
                           for (int j = 0; j < simplexData.tableaus[i].length; j++)
@@ -96,7 +218,14 @@ class SimplexProcessPage extends ConsumerWidget {
                                   DataCell(
                                     DecoratedBox(
                                       decoration: BoxDecoration(
-                                        color: cellColor(i, j, k),
+                                        color: getCellColor(
+                                          i,
+                                          j,
+                                          k,
+                                          simplexData.pivotsCoordinates,
+                                          simplexData.tableaus.length,
+                                          simplexData.tableaus[i][j].length,
+                                        ),
                                       ),
                                       child: SizedBox.expand(
                                         child: Padding(
@@ -107,11 +236,11 @@ class SimplexProcessPage extends ConsumerWidget {
                                                     textStyle: textTheme.titleLarge)
                                                 : Text(
                                                     simplexData.tableaus[i][j][k],
-                                              style: textTheme.bodyLarge!.copyWith(
-                                                fontFamily: 'CMRomanSerif',
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
+                                                    style: textTheme.bodyLarge!.copyWith(
+                                                      fontFamily: 'CMRomanSerif',
+                                                      fontWeight: FontWeight.bold,
+                                                    ),
+                                                  ),
                                           ),
                                         ),
                                       ),
