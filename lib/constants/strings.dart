53ae58d823ca049e88a4e7a70ffa49c9bef09105
OppositeDragon <stee-ven_94@outlook.com>
2023-12-28 19:47:42 -0500

    LPOR-13 - Created a share (link to problem) widget. - Add share functionality to pages.

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,5 +1,7 @@
 import 'package:linearprogrammingapp/constants/numeric.dart';
 
+const String authority = 'linear--programming.web.app';
+
 const String uploadHelpMessage1 =
     'Se debe proveer de un archivo en formato JSON, donde la cantidad de restricciones concuerda con la cantidad de operadores para formar las (in)equaciones. Además el maximo de variables es $maxVariables y el maximo de restricciones es $maxConstraints.';
 const String uploadHelpMessage2 = 'El objeto JSON de ejemplo, produce el resultado que se muestra a continuacion.';
